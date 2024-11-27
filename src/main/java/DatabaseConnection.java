bef585fac8efcd573f367f7377ef2baac8d7daba
shreya29k <141661274+shreya29k@users.noreply.github.com>
2024-11-27 21:38:19 +0530

    Update DatabaseConnection.java

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -1,1 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    public static Connection initializeDatabase() throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/user_access";
+        String user = "yourusername";
+        String password = "yourpassword";
+        return DriverManager.getConnection(url, user, password);
+    }
+}
 
